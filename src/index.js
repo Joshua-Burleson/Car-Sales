df26db983f57534aa323e1df4414a54ec37e26ca
Joshua-Burleson <joshua.burleson.developer@gmail.com>
2020-01-09 11:13:23 -0500

    MVP Completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { carReducer, initialState } from './reducers/carReducer';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(carReducer, initialState);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
